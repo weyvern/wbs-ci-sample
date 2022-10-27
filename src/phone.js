da8b2a6f225b56c922572fe9f422a06f8d47fda1
Jorge Paul <jserranopaul@gmail.com>
2022-10-27 14:44:58 +0200

    udpdated isPhoneNumber

--- src/phone.js
+++ src/phone.js
@@ -1,11 +1,9 @@
 export default function isPhoneNumber(number) {
-  number = number.replaceAll(" ", "");
-
-  // if (number.length === 13 && true) {
-  //   if (number.startsWith("+491")) {
-  //     return true;
-  //   }
-  // }
-
+  number = number.replaceAll(' ', '');
+  if (number.length === 13 && true) {
+    if (number.startsWith('+491')) {
+      return true;
+    }
+  }
   return false;
 }
